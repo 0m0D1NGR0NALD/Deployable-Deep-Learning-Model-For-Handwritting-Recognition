e0dc399f7a9351f1264dd6d246208047adeeb57a
OMODING RONALD <chidojawbreaker@gmail.com>
2023-03-13 12:59:27 +0300

    commit

---
+++ model.py
@@ -0,0 +1,5 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D
+from keras import backend as K
