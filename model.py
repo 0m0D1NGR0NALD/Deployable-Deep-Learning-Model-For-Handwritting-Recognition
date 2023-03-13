8b0e8a015100bee4cda827620bdde52dfb4353a4
OMODING RONALD <chidojawbreaker@gmail.com>
2023-03-13 13:37:03 +0300

    commit

--- model.py
+++ model.py
@@ -50,3 +50,8 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 
 # Training the Model
 model.fit(x_train,y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test,y_test))
+
+# Evaluating the predictions on the Model
+score = model.evaluate(x_test,y_test,verbose=0)
+print('Test Loss: ',score[0])
+print('Test Accuracy: ',score[1])
