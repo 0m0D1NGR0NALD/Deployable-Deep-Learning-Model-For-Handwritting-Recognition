f99a04711730c01ca8bf3852143ad65076a70e3f
OMODING RONALD <chidojawbreaker@gmail.com>
2023-03-13 13:04:43 +0300

    commit

--- model.py
+++ model.py
@@ -3,3 +3,13 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D
 from keras import backend as K
+
+# Defining hyperparameters
+num_classes = 10
+batch_size = 128
+epochs = 10
+
+# Image resolution
+rows,columns = 28,28
+# Loading the data
+(x_train,y_train),(x_train,y_test) = mnist.load_data()
